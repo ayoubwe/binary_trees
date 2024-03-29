66415f9e3bf47d396a2c4906d1e3b06fe309dc6e
Ayoub Douane <douaneayoubw@gmail.com>
2024-03-29 05:23:22 +0000

    4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,12 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if a node is a leaf
- * @node: node to check
+ * binary_tree_is_leaf - creating a function 
+ * to chek if a node is a leaf
+ * @node: for check
  *
  * Return: 1 if node is a leaf
- *         0 if not a leaf
- *         0 if node is NULL
+ *         & 0 if not a leaf
+ *         also 0 if node is NULL
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
