c21a39a6cd6216fc85725b48a4c8b4b9ca4bb18f
Ayoub Douane <douaneayoubw@gmail.com>
2024-03-29 05:41:25 +0000

    11&12

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: tree to measure the size of
+ * binary_tree_size - A function that measures
+ * the size of a binary tree
+ * @tree: is a pointer to the root node 
+ * of the tree to measure the size
  *
- * Return: size of the tree
- *         0 if tree is NULL
+ * Return: 0 if tree=Null
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - counts the leaves in a binary tree
- * @tree: tree to count the leaves from
+ * binary_tree_leaves - A function that counts
+ * the leaves in a binary tree
+ * @tree: is a pointer to the root node of the
+ * tree to measure the size
  *
- * Return: number of leaves
- *         0 if tree is NULL
+ * Return: 0 if tree=NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
